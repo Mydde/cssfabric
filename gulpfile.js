4db5bee63f2ee1117415dc3f2035df4b9ab2923f
mydde <lebrun.meddy@gmail.com>
2021-04-18 23:21:14 +0200

    _docs parser

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,40 @@
 const gulp = require("gulp"),
-  data = require("gulp-data"),
-   unescapeJs = require('unescape-js'),
+  unescapeJs = require("unescape-js"),
   jsonTransform = require("gulp-json-transform"),
-  // fabricRef = require("fabric-ref"),
-  fs = require("fs"),
-  // gulpData = require("css-fabric-data"),
-  through = require("through2");
-
-const spawn = require("cross-spawn");
-const jsonModify = require("gulp-json-modify");
+  spawn = require("cross-spawn");
 
 gulp.task("jsonComplete", function (cb) {
   gulp
     .src("./css-fabric/_config/**/*.json")
     .pipe(
       jsonTransform(function (file_content, file_info) {
-    
-        return da_callback({ file_content: unescapeJs(file_content), file_info });
-      }, "\n")
+        return da_callback({ file_content: file_content, file_info });
+      }, "\t")
     )
     .pipe(gulp.dest("./css-fabric/_generated"));
+
+  cb();
 });
 
 function da_callback(ard) {
   const { file_content, file_info } = ard;
-  const module_name = file_info.relative.split("\git status\")?.pop().split(".")?.[0];
+  file_info.relative = unescapeJs(file_info.relative);
+  const module_name = file_info.relative.split("\\")?.pop().split(".")?.[0];
 
   if (file_content?.[module_name]) {
     let module_conf = file_content[module_name];
 
     if (module_conf) {
-      console.log({ module_conf });
       const module_data = module_conf?.["_data"] || {};
 
-     // if (!module_conf?.["_docs"]) {
-        file_content[module_name]._docs = {init:true,lo:Object.keys(module_data)};
-     // }
+      console.log({ module_data });
+
+      if (!module_conf?.["_docs"]) module_conf["_docs"] = {};
+      const out_docs = {};
+      Object.keys(module_data).forEach((v, i, a) => {
+        if (!module_conf?.["_docs"]?.[v]) out_docs[v] = "";
+      });
+      file_content[module_name]._docs = out_docs;
     }
   } else {
     console.log(
