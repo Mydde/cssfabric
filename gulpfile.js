1987d2e9f9f6825c35ac38d747cdb2ff3f5eeb1c
mydde <lebrun.meddy@gmail.com>
2021-04-19 20:08:17 +0200

    gulp: key not in _data are marked as deleted

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,8 @@
 const gulp          = require("gulp"),
       unescapeJs    = require("unescape-js"),
       jsonTransform = require("gulp-json-transform"),
-      cache = require('gulp-cached'),
+      cache         = require('gulp-cached'),
       spawn         = require("cross-spawn");
-
-
 
 /**
  * 
@@ -20,23 +18,36 @@ const json_comments = (file) =>{
   const module_name = file_info.relative.split("\\")?.pop().split(".")?.[0];
 
   if (file_content?.[module_name]) {
-    let module_conf = file_content[module_name];
+    let module_conf = file_content[module_name]; 
 
     if (module_conf) {
       const module_data = module_conf?.["_data"] || {};
- 
+      const module_docs = module_conf?.["_docs"] || {}; 
 
       if (!module_conf?.["_docs"]) module_conf["_docs"] = {};
+
       const out_docs = {};
       let out_docs_changed = false;
       
       Object.keys(module_data).forEach((v, i, a) => {
-        if (!module_conf?.["_docs"]?.[v]) {
-         //  console.log({ module_data });
-          out_docs[v] = "_";
+        if (!module_conf?.["_docs"]?.[v]) { 
+          out_docs[v] = "";
           out_docs_changed = true;
         }
       });
+
+      Object.keys(module_docs).forEach((k, i, a) => {
+          // :undefined because boolean values to test for
+         if(module_data?.[k] === undefined){ 
+            if(!Boolean(module_docs[k].length) || module_docs?.[k] !== "deleted:"){
+              out_docs[k] = "deleted:" + module_docs[k];
+            }else{
+              out_docs[k] = undefined,
+              delete( out_docs[k]);
+            }
+            out_docs_changed = true;
+          }
+      })
 
       if(out_docs_changed)   file_content[module_name]._docs = Object.assign(file_content[module_name]._docs,out_docs)  ;
     }
@@ -47,11 +58,9 @@ const json_comments = (file) =>{
   return { ...file_content };
 }
 
-
-
 const fabricRootDir   = './css-fabric',
       fabricStylesDir = 'styles',
-      fabricConfDir   = `${fabricRootDir}/_test_config`,
+      fabricConfDir   = `${fabricRootDir}/_config`,
       fabricModuleDir = `${fabricRootDir}/modules`,
       generatedDir    = `${fabricRootDir}/_generated`;
 
@@ -108,7 +117,7 @@ function watchJsonTask(cb) {
 
 function watchSassTask(cb) {   
 
-  gulp.watch("./css-fabric", task_sass2css);
+  gulp.watch(fabricRootDir, task_sass2css);
 
   cb();
 }
