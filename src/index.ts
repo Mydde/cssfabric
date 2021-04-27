27cef81a08928a73ef9be367e5543c51b2dd4b2b
mydde <lebrun.meddy@gmail.com>
2021-04-27 23:48:50 +0200

    done some tweaking

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import jsonConfig from "../lib/css-fabric/_generated/css.fabric.config.json";
-import cssFile from "../lib/styles/css-fabric/core/css-fabric.css";
+import jsonConfig from "../src/css-fabric/_generated/css.fabric.config.json";
+/* import cssFile from "../src/styles/css-fabric/core/css-fabric.css"; */
 
 const getCssFile = () => { 
-  return cssFile;
+  return 'cssFile';
 };
 
-const getJsonConfig = (module?: string) => {
-  if (module) return jsonConfig["css-config"]["modules"][module] || {};
+const getJsonConfig = (module?: string) => { 
+  if (module) return jsonConfig["css-config"]["modules"][module] || {}; 
   return jsonConfig;
 };
 
