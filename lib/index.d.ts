4372ad2c52dbd2d67570a11a4ec08a33847027ef
mydde <lebrun.meddy@gmail.com>
2021-04-29 18:33:19 +0200

    feat: base keyand placeholders

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,20 @@
+export interface IFabricConfModulePart {
+    [key: string]: any;
+}
+export interface IFabricConfModuleDataPart {
+    [key: string]: any;
+}
+export interface IFabricConfModuleMetaDataPart {
+    [key: string]: any;
+}
+export interface IFabricConfModuleDocsPart {
+    [key: string]: any;
+}
 declare const _default: {
     getSassConfig: () => string;
-    getJsonConfig: (module?: string) => any;
+    getModuleConfig: (module?: string) => IFabricConfModulePart;
+    getModuleData: (module?: string) => IFabricConfModuleDataPart;
+    getModuleMetaData: (module?: string) => IFabricConfModuleMetaDataPart;
+    getModuleDocs: (module?: string) => IFabricConfModuleDocsPart;
 };
 export default _default;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,55 @@
 import jsonConfig from "../src/css-fabric/_generated/css.fabric.config.json";
 /* import cssFile from "../src/styles/css-fabric/core/css-fabric.css"; */
 
+export interface IFabricConfModulePart {
+  [key: string]: any;
+}
+
+export interface IFabricConfModuleDataPart {
+  [key: string]: any;
+}
+
+export interface IFabricConfModuleMetaDataPart {
+  [key: string]: any;
+}
+export interface IFabricConfModuleDocsPart {
+  [key: string]: any;
+}
+
 const getCssFile = () => { 
-  return 'cssFile';
+  return "cssFile";
 };
 
-const getJsonConfig = (module?: string) => { 
-  if (module) return jsonConfig["css-config"]["modules"][module] || {}; 
+const getModuleConfig = (module?: string):IFabricConfModulePart => {
+  if (module) return jsonConfig["css-config"]?.["modules"]?.[module] || {};
   return jsonConfig;
 };
 
-export default { getSassConfig: getCssFile, getJsonConfig }; 
+const getModuleData = (module?: string):IFabricConfModuleDataPart => {
+  if (module) return jsonConfig["css-config"]?.["modules"]?.[module]?.["_data"] || {};
+  return jsonConfig;
+};
+
+const getModuleMetaData = (module?: string):IFabricConfModuleMetaDataPart => {
+  if (module)
+    return jsonConfig["css-config"]?.["modules"]?.[module]?.["_metadata"] || {};
+  return jsonConfig;
+};
+/**
+ * 
+ * @param module does the job
+ * @returns 
+ */
+const getModuleDocs = (module?: string):IFabricConfModuleDocsPart => {
+  if (module)
+    return jsonConfig["css-config"]?.["modules"]?.[module]?.["_metadata"] || {};
+  return jsonConfig;
+};
+
+export default {
+  getSassConfig: getCssFile,
+  getModuleConfig,
+  getModuleData, 
+  getModuleMetaData,
+  getModuleDocs
+};
