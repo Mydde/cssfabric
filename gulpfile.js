959e32dde0ebac9252b431a5a1f68d7559fb2adf
mydde <lebrun.meddy@gmail.com>
2021-05-05 01:51:41 +0200

    bug: overflow sys

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,25 @@
 // ## WIP
 
-const gulp = require("gulp"),
-    unescapeJs = require("unescape-js"),
-    jsonTransform = require("gulp-json-transform"),
-    cache = require("gulp-cached"),
-    gulpSass = require("gulp-sass"),
-    // sassExport = require("gulp-sass-export"),
-    mergeJson = require("gulp-merge-json"),
-    gulFileList = require("gulp-filelist"),
-    minifyCss = require("gulp-minify-css"),
-    fs = require("fs"),
-    gulpRename = require("gulp-rename"),
-    gulpDownload = require("gulp-download-stream"),
-    gulpConcat = require("gulp-concat-util"),
-    sassJson = require("gulp-sass-json"),
-    sassExport = require("gulp-sass-export"),
-    sassVarsToJs = require("gulp-sass-vars-to-js"),
-    parse = require("sass-parser")(),
-    modifyFile = require("gulp-modify-file");
-gulpIgnore = require("gulp-ignore");
+const gulp             = require("gulp"),
+      unescapeJs       = require("unescape-js"),
+      jsonTransform    = require("gulp-json-transform"),
+      cache            = require("gulp-cached"),
+      gulpSass         = require("gulp-sass"),
+      // sassExport = require("gulp-sass-export"),
+      mergeJson        = require("gulp-merge-json"),
+      gulFileList      = require("gulp-filelist"),
+      minifyCss        = require("gulp-minify-css"),
+      fs               = require("fs"),
+      gulpRename       = require("gulp-rename"),
+      gulpDownload     = require("gulp-download-stream"),
+      gulpConcat       = require("gulp-concat-util"),
+      sassJson         = require("gulp-sass-json"),
+      sassExport       = require("gulp-sass-export"),
+      sassVarsToJs     = require("gulp-sass-vars-to-js"),
+      parse            = require("sass-parser")(),
+      gulpJsBeautifier = require('gulp-jsbeautifier'),
+      modifyFile       = require("gulp-modify-file");
+gulpIgnore             = require("gulp-ignore");
 
 var spawn = require("child_process").spawn;
 
@@ -27,39 +28,39 @@ gulpSass.compiler = require("sass");
 var fabricConfig = require("./cssfabric.json");
 
 const {
-    fabricRootDir,
-    fabricStylesDir,
-    fabricConfDir,
-    fabricModuleDir,
-    fabricGeneratedDir,
-} = fabricConfig;
+          fabricRootDir,
+          fabricStylesDir,
+          fabricConfDir,
+          fabricModuleDir,
+          fabricGeneratedDir,
+      } = fabricConfig;
 
 function fabricReadmeFile(filePath) {
     // name of the module, from path
     let module_name = filePath
         .substring(filePath.lastIndexOf("modules/"))
         .replace("modules/", "");
-
+    
     let module = filePath.substring(filePath.lastIndexOf("/") + 1);
-
+    
     let out = "### " + module_name + "" + "\r\n";
-
+    
     return out;
 }
 
 const sassJsonExporter = (file) => {
     let {file_content, file_info} = file;
-
+    
     let obj;
     obj = file_content.obj;
     obj = obj.substring(0, obj.length - 1).split("|");
-
-    const redPath = "";
+    
+    const redPath       = "";
     const redModulePath = fabricModuleDir + "/";
-
-    let header = (footer = "");
+    
+    let header       = (footer = "");
     let importExport = `@import  "./src/${redPath}vendor/sass-json-export/stylesheets/sass-json-export.scss";`;
-
+    
     Object.values(obj).forEach((v, k, a) => {
         let module_path = redModulePath + v;
         let module_name = v
@@ -68,35 +69,35 @@ const sassJsonExporter = (file) => {
             ?.pop()
             .split(".")?.[0]
             .replace("-vars", "");
-
+        
         if (v) {
             header += makeHeader(module_path, module_name);
             footer += makeFooter(module_name);
         }
     });
-
+    
     let out = header;
     out += "\r\n";
     out += importExport + "\r\n";
     out += "\r\n";
     out += footer;
-
+    
     function makeHeader(path, module_name) {
         // form is module-vars.$module-config
         return '@use "' + path + '.scss" as  ' + module_name + "; \r\n";
     }
-
+    
     function makeFooter(module_name) {
         // form is module.$module-(config|*)
         //  $export : ( data: base-vars.$base-config, docs :base-vars.$base-docs , metadata :base-vars.$base-metadata  );
-
+        
         let out1 = ` ( _data: ${module_name}.$${module_name}-config, _docs :${module_name}.$${module_name}-docs , _metadata :${module_name}.$${module_name}-metadata  )`;
-
+        
         return (
             "@include json-encode(" + out1 + ",comment," + module_name + ");\r\n"
         );
     }
-
+    
     return out;
 };
 
@@ -108,7 +109,7 @@ function fabricVarExportFile(filePath) {
         ?.pop()
         .split(".")?.[0]
         .replace("modules/", "");
-
+    
     return module_name + "|";
 }
 
@@ -117,19 +118,19 @@ function fabricCssVarExportFile(filePath) {
         .split("modules/")[1]
         .replace("modules", "")
         .replace(".scss", "");
-
+    
     let module_filename = filePath
         .substring(filePath.lastIndexOf("/"))
         .replace(".scss", "")
         .replace("/", "");
-
+    
     let module_name = filePath
         .substring(filePath.lastIndexOf("/_"))
         .replace(".scss", "")
         .replace("-vars", "")
         .replace("_", "")
         .replace("/", "");
-
+    
     return `|{"module_path" : "${module_path}","module_filename" : "${module_filename}","module_name" : "${module_name}"}`;
 }
 
@@ -140,23 +141,23 @@ function task_cssVarsExport(cb) {
         .src(sourceFiles)
         .pipe(
             gulFileList("cssfabric-vars.css", {
-                destRowTemplate: fabricCssVarExportFile,
+                destRowTemplate:  fabricCssVarExportFile,
                 removeExtensions: false,
             })
         )
         .pipe(
             modifyFile((content, path, file) => {
-                let exp = content.split("|"); // JSON.parse()
+                let exp    = content.split("|"); // JSON.parse()
                 let header = (footer = "");
-
-                let utils = `@use '${fabricRootDir}/utils' as utils;` + "\r\n";
-                const openVarTag = ":root{";
+                
+                let utils         = `@use '${fabricRootDir}/utils' as utils;` + "\r\n";
+                const openVarTag  = ":root{";
                 const closeVarTag = "}";
-
+                
                 Object.values(exp).forEach((v, k, a) => {
                     if (v) {
                         moduleConf = JSON.parse(v);
-
+                        
                         if (moduleConf) {
                             header +=
                                 `@use '${fabricModuleDir}/${moduleConf.module_path}' as ${moduleConf.module_name};` +
@@ -167,9 +168,9 @@ function task_cssVarsExport(cb) {
                         }
                     }
                 });
-
+                
                 const out = utils + header + openVarTag + footer + closeVarTag;
-
+                
                 return out;
             })
         )
@@ -183,13 +184,13 @@ function task_cssVarsExport(cb) {
 // exports sass maps to json
 function task_varsExport(cb) {
     let sourceFiles = fabricModuleDir + "/**/_*-vars.scss";
-
+    
     gulp
         .src(sourceFiles)
         //.pipe(cache(task_varsExport))
         .pipe(
             gulFileList("ghost", {
-                destRowTemplate: fabricVarExportFile,
+                destRowTemplate:  fabricVarExportFile,
                 removeExtensions: false,
             })
         )
@@ -199,7 +200,7 @@ function task_varsExport(cb) {
         .pipe(
             jsonTransform(function (file_content, file_info) {
                 return sassJsonExporter({file_content: file_content, file_info});
-            }, "\t")
+            })
         )
         .pipe(cache(task_varsExport))
         // .pipe(sass().on('error', sass.logError))
@@ -207,23 +208,24 @@ function task_varsExport(cb) {
         .pipe(
             modifyFile((content, path, file) => {
                 const start = '{"cssfabric":{"modules":{';
-                const end = " }}}";
-
-                const regexIn = /\/\*\! json-encode: {/gm;
+                const end   = " }}}";
+                
+                const regexIn  = /\/\*\! json-encode: {/gm;
                 const regexOut = /} \*\//gm;
-
+                
                 let exp = content
                     .replace(regexIn, "")
                     .replace(regexOut, ",")
                     .replace(/,\s*$/, "");
-
+                
                 return `${start}${exp}${end}`;
             })
         )
+        .pipe(gulpJsBeautifier())
         .pipe(
             gulpRename(function (path) {
-                path.dirname = path.dirname;
-                path.extname = ".json";
+                path.dirname  = path.dirname;
+                path.extname  = ".json";
                 path.basename = path.basename.replace("-", ".");
             })
         )
@@ -231,7 +233,7 @@ function task_varsExport(cb) {
         .on("end", () => {
             return cb();
         });
-
+    
     return cb();
 }
 
@@ -240,7 +242,7 @@ function task_readme(cb) {
         .src(fabricModuleDir + "/*/*[!_].scss")
         .pipe(
             gulFileList("readme.md", {
-                destRowTemplate: fabricReadmeFile,
+                destRowTemplate:  fabricReadmeFile,
                 removeExtensions: true,
             })
         )
@@ -260,17 +262,17 @@ function task_readme(cb) {
 function task_mergeInclude(cb) {
     //
     const dest = fabricStylesDir;
-    const dir = fabricStylesDir + "/core";
-
+    const dir  = fabricStylesDir + "/core";
+    
     const steps = [];
     // normal stylesheets
     steps.push(
         gulp
             .src([
-                `${dir}/**/*.css`,
-                `!${dir}/**/*responsive*.css`,
-                `!${dir}/**/*min*.css`,
-            ])
+                     `${dir}/**/*.css`,
+                     `!${dir}/**/*responsive*.css`,
+                     `!${dir}/**/*min*.css`,
+                 ])
             .pipe(gulpConcat("cssfabric.css"))
             .pipe(gulpConcat.header("/** Merged by Mydde */"))
             .pipe(cache(task_mergeInclude))
@@ -279,7 +281,7 @@ function task_mergeInclude(cb) {
                 return cb();
             })
     );
-
+    
     // normal minified stylesheets
     steps.push(
         gulp
@@ -291,7 +293,7 @@ function task_mergeInclude(cb) {
                 return cb();
             })
     );
-
+    
     // responsive stylesheets
     steps.push(
         gulp
@@ -303,7 +305,7 @@ function task_mergeInclude(cb) {
                 return cb();
             })
     );
-
+    
     // responsive minified stylesheets
     steps.push(
         gulp
@@ -315,7 +317,7 @@ function task_mergeInclude(cb) {
                 return cb();
             })
     );
-
+    
     return [...steps];
 }
 
@@ -336,8 +338,8 @@ function task_sass2css(cb) {
             .pipe(gulpIgnore.exclude("**/*css-fabric*"))
             .pipe(
                 gulpRename(function (path) {
-                    path.dirname = path.dirname;
-                    path.extname = path.extname;
+                    path.dirname  = path.dirname;
+                    path.extname  = path.extname;
                     path.basename = path.basename.replace("-", ".");
                 })
             )
@@ -349,13 +351,13 @@ function task_sass2css(cb) {
             // to css and minify and to /core
             .pipe(
                 minifyCss({
-                    keepSpecialComments: 0,
-                })
+                              keepSpecialComments: 0,
+                          })
             )
             .pipe(
                 gulpRename(function (path) {
-                    path.dirname = path.dirname;
-                    path.extname = ".min.css";
+                    path.dirname  = path.dirname;
+                    path.extname  = ".min.css";
                     path.basename = path.basename.replace("-", ".");
                 })
             )
@@ -382,7 +384,7 @@ function watchSassTask(cb) {
         fabricModuleDir + "/**/*.scss",
         gulp.series(task_sass2css, task_mergeInclude, watchCssExportVars, task_varsExport)
     ); // task_varsExport
-
+    
     cb();
 }
 
@@ -390,7 +392,7 @@ function watchSassTask(cb) {
 function watchInclude(cb) {
     // gulp.watch(fabricStylesDir, task_mergeInclude);
     //gulp.watch(fabricModuleDir, task_mergeInclude);
-
+    
     cb();
 }
 
@@ -400,27 +402,27 @@ function watchReadme(cb) {
         [fabricModuleDir, "!" + fabricModuleDir + "/**/_*.scss"],
         task_readme
     );
-
+    
     cb();
 }
 
 function watchExportVars(cb) {
     gulp.watch(fabricRootDir, gulp.series(task_varsExport)); // task_varsExport
-
+    
     cb();
 }
 
 function watchCssExportVars(cb) {
     gulp.watch(fabricModuleDir, gulp.series(task_cssVarsExport)); // task_varsExport
-
+    
     cb();
 }
 
 // oly one called by npm
 exports.watchSass = watchSassTask;
 
-exports.watchInclude = watchInclude;
-exports.watchReadme = watchReadme;
+exports.watchInclude    = watchInclude;
+exports.watchReadme     = watchReadme;
 exports.watchExportVars = watchExportVars;
 
 exports.watchCssExport = watchCssExportVars;
